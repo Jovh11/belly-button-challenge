940dc1fcfa90109e0fd66178111ccf3e0ff9a31f
Jovh11 <93451967+Jovh11@users.noreply.github.com>
2022-09-28 15:01:25 -0400

    Finished Base work

--- data.js
+++ data.js
@@ -5,9 +5,12 @@ var information;
 var desiredInfo;
 d3.json(url).then(function(data){
     information = data;
+    demographics = information.metadata;
     desiredInfo = information.samples;
     test = desiredInfo[0];
+    test2 = demographics[0];
     plotter(test);
+    demoInfo(test2);
 });
 
 // console.log(desiredInfo);
--- plot.js
+++ plot.js
@@ -25,18 +25,52 @@ function plotter (data) {
         type: 'bar',
         orientation: 'h'
     };
+    let bub_x = data.otu_ids;
+    let bub_y = data.sample_values;
+    bub_x.reverse();
+    bub_y.reverse();
+    let trace2 = {
+        x: bub_x,
+        y: bub_y,
+        xaxis: 'x2',
+        yaxis: 'y2',
+        text: data.otu_labels,
+        mode: 'markers',
+        marker: {
+            size: data.sample_values,
+            color: data.otu_ids,
+        }
+    };
     traceData = [trace1];
+    traceData2 = [trace2];
     console.log(trace1.x);
     console.log(trace1.y);
     layout = {
-        title: 'Top 10 Sample Values'
+        title: 'Top 10 Sample Values',
+        // grid: {rows: 1, columns: 2, pattern: 'independent', roworder: 'bottom to top'}
     };
 
-    Plotly.newPlot('plot', traceData, layout);
-    Plotly.newPlot('plot', traceData, layout);
+    Plotly.newPlot('bar', traceData, layout);
+    Plotly.newPlot('bubble', traceData2, layout);
+
 }
 
+function demoInfo(data2) {
+        var keys = Object.keys(data2);
+        var values = Object.values(data2);
+        var ol = document.getElementById('metalist');
+        // ol.createElement('ol');
+        for (let i = 0; i < keys.length; i++) {
+            let li = document.createElement('li');
+            li.innerText=`${keys[i]}: ${values[i]}`;
+            ol.appendChild(li);
+        }
+        // document.getElementById('sample-metadata').appendChild(ol);
+    };
 
+// Create the HTML list var ol = document.createElement("ol");
+// Loop through the array and append list items for (let i of ARRAY) { let li = document.createElement("li"); li.innerHTML = i; ol.appendChild(li); }
+// Append the list to where you want document.getElementById("TARGET").appendChild(ol);
 
 
 
